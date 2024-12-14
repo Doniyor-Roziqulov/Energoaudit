f8ebbcdd01bbfbebfdda88e3a3c2c0945daba930
Doniyor Ro'ziqulov <doniyorruziqulov33@gmail.com>
2024-12-14 21:21:43 +0500

    Added

--- src/components/use/Use.jsx
+++ src/components/use/Use.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Button, Modal } from "antd";
 
 import logo1 from "@/images/midea.png";
 import logo2 from "@/images/tdtu.png";
@@ -27,6 +28,50 @@ const Use = () => {
     });
   };
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const [isModalOpen2, setIsModalOpen2] = useState(false);
+  const showModal2 = () => {
+    setIsModalOpen2(true);
+  };
+  const handleOk2 = () => {
+    setIsModalOpen2(false);
+  };
+  const handleCancel2 = () => {
+    setIsModalOpen2(false);
+  };
+
+  const [isModalOpen3, setIsModalOpen3] = useState(false);
+  const showModal3 = () => {
+    setIsModalOpen3(true);
+  };
+  const handleOk3 = () => {
+    setIsModalOpen3(false);
+  };
+  const handleCancel3 = () => {
+    setIsModalOpen3(false);
+  };
+
+  const [isModalOpen4, setIsModalOpen4] = useState(false);
+  const showModal4 = () => {
+    setIsModalOpen4(true);
+  };
+  const handleOk4 = () => {
+    setIsModalOpen4(false);
+  };
+  const handleCancel4 = () => {
+    setIsModalOpen4(false);
+  };
+
   return (
     <section className="overflow-x-hidden pb-20">
       <div className="container mx-auto max-w-[1385px] px-5">
@@ -35,7 +80,7 @@ const Use = () => {
         </h2>
         <div className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 lg:grid-cols-4 gap-x-5 mb-20">
           <button
-            onClick={() => handleToggle(1)}
+            onClick={(() => handleToggle(1), showModal)}
             className="py-3 bg-[#fff] hover:bg-[#0A9642] use__box hover:scale-105 hover:z-20 relative cursor-pointer flex items-center justify-center px-0"
           >
             <p className="text__use">Tajriba va ishonch</p>
@@ -56,8 +101,24 @@ const Use = () => {
               </p>
             </div>
           </button>
+          <Modal
+            title="Tajriba va ishonch"
+            open={isModalOpen}
+            onOk={handleOk}
+            footer={null}
+            onCancel={handleCancel}
+          >
+            <p className="">
+              • Energiya va energoaudit sohasida tajriba va ishonch juda muhim
+              omillar hisoblanadi. Ushbu sohalar aniq hisob-kitoblar,
+              samaradorlik tahlillari va muammolarni hal qilishni talab
+              qiladigan murakkab texnologik jarayonlarni o‘z ichiga oladi.
+              Tajriba va ishonch bu jarayonlarda yuqori natijalarga erishish
+              uchun asosiy poydevor hisoblanadi.
+            </p>
+          </Modal>
           <button
-            onClick={() => handleToggle(2)}
+            onClick={(() => handleToggle(2), showModal2)}
             className="py-3 bg-[#fff] hover:bg-[#0A9642] use__box hover:scale-105 hover:z-20 relative cursor-pointer flex items-center justify-center px-0"
           >
             <p className="text__use">Sifat</p>
@@ -76,9 +137,24 @@ const Use = () => {
                 bo'lishni anglatadi.
               </p>
             </div>
-          </button>
+          </button>{" "}
+          <Modal
+            title="Sifat"
+            open={isModalOpen2}
+            onOk={handleOk2}
+            footer={null}
+            onCancel={handleCancel2}
+          >
+            <p className="">
+              • Sifat — bu mahsulot, xizmat yoki jarayonning belgilangan talab
+              va ehtiyojlarga muvofiqligini tavsiflovchi asosiy xususiyatdir. Bu
+              tushuncha turli sohalarda turli ma'nolarni anglatishi mumkin,
+              lekin ularning barchasida sifatlilik yuqori darajada qoniqarli
+              bo'lishni anglatadi.
+            </p>
+          </Modal>
           <button
-            onClick={() => handleToggle(3)}
+            onClick={(() => handleToggle(3), showModal3)}
             className="py-3 bg-[#fff] hover:bg-[#0A9642] use__box hover:scale-105 hover:z-20 relative cursor-pointer flex items-center justify-center px-0"
           >
             <p className="text__use">Malakali mutaxassislar</p>
@@ -98,8 +174,23 @@ const Use = () => {
               </p>
             </div>
           </button>
+          <Modal
+            title="Malakali mutaxassislar"
+            open={isModalOpen3}
+            onOk={handleOk3}
+            footer={null}
+            onCancel={handleCancel3}
+          >
+            <p className="">
+              • Malakali mutaxassislar - bu o‘z sohasida chuqur bilim va
+              tajribaga ega bo‘lgan, aniq maqsadlar va yuqori sifatli
+              natijalarga erishishni ta'minlaydigan professional kadrlar
+              hisoblanadi. Energiya va energoaudit sohasida malakali
+              mutaxassislarning o‘rni juda muhimdir.
+            </p>
+          </Modal>
           <button
-            onClick={() => handleToggle(4)}
+            onClick={(() => handleToggle(4), showModal4)}
             className="py-3 bg-[#fff] hover:bg-[#0A9642] use__box hover:scale-105 hover:z-20 relative cursor-pointer flex items-center justify-center px-0"
           >
             <p className="text__use">O’lchov asboblari majmuasi</p>
@@ -119,6 +210,21 @@ const Use = () => {
               </p>
             </div>
           </button>
+          <Modal
+            title="O‘lchov asboblari majmuasi"
+            open={isModalOpen4}
+            onOk={handleOk4}
+            footer={null}
+            onCancel={handleCancel4}
+          >
+            <p className="">
+              • Energiya va energoaudit sohasida o‘lchov asboblari majmuasi
+              asosan ob'ektning energiya sarfi, samaradorligi va atrof-muhitga
+              ta'sirini aniqlash uchun zarur bo‘lgan texnologik vositalarni
+              anglatadi. Ushbu asboblar aniq o‘lchovlar va tahlillar uchun
+              asosiy vositalar bo‘lib xizmat qiladi.
+            </p>
+          </Modal>
         </div>
         <div
           className="flex items-center py-8 lg:py-0 flex-col sm:flex-row overflow-x-hidden flex-wrap
